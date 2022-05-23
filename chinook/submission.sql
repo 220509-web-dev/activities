42a08351faf0aa1147bdbe8dd34f6616d10cf6ad
Almustafa James <almustafa830@revature.net>
2022-05-23 18:04:29 -0500

    started chinook activity

---
+++ chinook/submission.sql
@@ -0,0 +1,5 @@
+-- Task – Select all records from the Employee table.
+SELECT * FROM "Employee";
+
+-- Task – Select all records from the Employee table where last name is King.
+select * from "Employee" where "LastName"  is King;
