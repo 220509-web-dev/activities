e7faa6eafc23419a567070f9a31f8c533af35edd
Almustafa James <almustafa830@revature.net>
2022-05-23 18:17:07 -0500

    started chinook activity

--- chinook/submission.sql
+++ chinook/submission.sql
@@ -2,4 +2,10 @@
 SELECT * FROM "Employee";
 
 -- Task – Select all records from the Employee table where last name is King.
-select * from "Employee" where "LastName"  is King;
+select * from "Employee" where "LastName" = 'King';
+
+-- Task – Select all records from the Employee table where first name is Andrew and REPORTSTO is NULL.
+select * from "Employee" where "FirstName" = 'Andrew' and "ReportsTo" is null;
+
+-- Task – Select all albums in album table and sort result set in descending order
+select * from "Album" order by "AlbumId" desc;
